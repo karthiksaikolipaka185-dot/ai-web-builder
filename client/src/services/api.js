67aa9d25c04cd3c05f987f709dd9fd7f8bcc3d2c
KARTHIK SAI <kolipakakarthiksai603@gmail.com>
2026-04-04 22:52:18 +0530

    fixed api baseURL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://ai-web-builder-1-8bm9.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
